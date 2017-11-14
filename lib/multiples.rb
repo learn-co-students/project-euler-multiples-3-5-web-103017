82cea5c67589993f5c58dd636c82ba1b2631d1ca
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-14 14:14:47 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,29 @@
 # Enter your procedural solution here!
+# def multiples
+#   i = 0
+#   sum = 0
+# while i != 1000
+#   i += 1
+#   if i%3==0 || i%5==0
+#     sum += i
+#   end
+# end
+# sum
+# end
+
+def collect_multiples(limit)
+  i = 0
+  sum = []
+  while i != limit-1
+    i += 1
+    if i%3==0 || i%5==0
+      sum << i
+    end
+  end
+  sum
+end
+
+def sum_multiples(limit)
+  multiples = collect_multiples(limit)
+  multiples.inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,25 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    i = 0
+    sum = []
+    while i != @limit-1
+      i += 1
+      if i%3==0 || i%5==0
+        sum << i
+      end
+    end
+    sum
+  end
+
+  def sum_multiples
+    multiples = collect_multiples
+    multiples.inject(:+)
+  end
+
+end
