fa42bf8e6224fca2a4918f125946329e71216f64
eshim12 <ellishim@gmail.com>
2017-11-14 09:53:24 -0500

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,25 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :value
+  def initialize(value)
+    @value = value
+  end
+
+  def collect_multiples
+    i = 1
+    num = []
+    while i < self.value
+      if i % 3 == 0 || i % 5 == 0
+        num << i
+        i += 1
+      else
+        i += 1
+      end
+    end
+    num
+  end
+
+  def sum_multiples
+    collect_multiples.reduce(:+)
+  end
+end
