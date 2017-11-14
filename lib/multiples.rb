60d377541b2f4922af7bf0cad8f4ddf2ed24b772
dansbands <dansbands@hotmail.com>
2017-11-14 11:03:31 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+  nums = (1...limit)
+
+  nums.select do |num|
+    num % 3 == 0 || num % 5 == 0
+  end
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(:+)
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,20 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    nums = (1...limit)
+
+    nums.select do |num|
+      num % 3 == 0 || num % 5 == 0
+    end
+  end
+
+  def sum_multiples
+    collect_multiples.inject(:+)
+  end
+end
