1aa6c6e9ddcb5b0307ac4a1a339647d6d262fbf0
github username <github email address>
2017-11-14 09:58:02 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,10 @@
 # Enter your procedural solution here!
+# (1..100).inject(0) {|sum, num| (num % 3 == 0 || num % 5 == 0) ? num + sum : sum}
+
+def collect_multiples(limit)
+  (1...limit).select {|x| x % 3 == 0|| x % 5 == 0}
+end
+
+def sum_multiples(limit)
+  collect_multiples(limit).inject(:+)
+end
