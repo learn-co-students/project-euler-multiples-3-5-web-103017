ef39ce2d0f0a6dcb7fd522266cea78f2a5a292f1
psarma89 <priyam.sarma@gmail.com>
2017-11-14 09:55:28 -0500

    Adds Solution

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
 # Enter your procedural solution here!
+require 'pry'
+
+def collect_multiples(limit)
+  array = Array(3..(limit-1))
+  array.select{|x| x if (x % 3 == 0 || x % 5 == 0)}
+end
+
+  # array.inject(0) do |sum,x|
+  #   # binding.pry
+  #   sum + x if (x % 3 == 0 || x % 5 == 0)
+  #   sum
+  # end
+def sum_multiples(limit)
+  collect_multiples(limit).inject(0){|sum,x| sum +x}
+    # binding.pry
+end
+
+# puts multiples(1000)
