4d450e3e5e5a211d87f83b8b1cac5f95f653d5c3
c0nniewang <connie.wang303@gmail.com>
2017-11-14 09:28:45 -0500

    done

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,16 @@
-# Enter your object-oriented solution here!
+require 'pry'
+class Multiples
+  attr_accessor :n
+
+  def initialize(n)
+    @n = n
+  end
+
+  def collect_multiples
+    (1...n).select {|el| el % 3 == 0 || el % 5 == 0}
+  end
+
+  def sum_multiples
+    self.collect_multiples.reduce(:+)
+  end
+end
