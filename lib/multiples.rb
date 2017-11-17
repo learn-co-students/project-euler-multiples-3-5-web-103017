d77f1bcde5601600753dd34abf4e500316c6aab4
pawlkris <paulkristapovich@gmail.com>
2017-11-17 10:04:56 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,8 @@
 # Enter your procedural solution here!
+def collect_multiples(limit)
+  (1...limit).to_a.select{|x|x%3==0 || x%5==0}
+end
+
+def sum_multiples(limit)
+  (1...limit).to_a.inject(0){|sum,x|(x%3==0 || x%5==0) ? sum += x : sum}
+end
